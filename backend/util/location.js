db65d41210965abae9d241ff127e7309afdf3b0e
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-12 11:39:07 +0600

    custom http-hook used for creating new places

--- backend/util/location.js
+++ backend/util/location.js
@@ -5,10 +5,10 @@ const HttpError = require('../models/http-error');
 const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
 
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516
+  };
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -63,7 +63,7 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/login',
           'POST',
           JSON.stringify({
@@ -73,16 +73,15 @@ const Auth = () => {
           {
             'Content-Type': 'application/json'
           }
-
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {
 
       }
       // setIsLoading(false);
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
           JSON.stringify({
@@ -95,7 +94,7 @@ const Auth = () => {
           }
         );
 
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) { }
 
     }
