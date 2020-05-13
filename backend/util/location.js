ecef33096654ad37fbbc600c210b773aaf114c06
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-13 13:39:12 +0600

    places per user is now working

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = 'AIzaSyCDsl3eVL9I9OJqrkDPWE2-7fux35cShJE';
 
 async function getCoordsForAddress(address) {
   return {
