a0470849064e44b5cc4b70d0484060571350e354
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-14 11:55:38 +0600

    delete place is now working

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -24,6 +24,10 @@ const UserPlaces = () => {
     fetchPlaces();
   }, [sendRequest, userId]);
 
+  const placeDeletedHandler = (deletedPlaceId) =>{
+    setLoadedPlaces(prevPlaces => prevPlaces.filter(place=>place.id !== deletedPlaceId));
+  };
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
@@ -32,7 +36,7 @@ const UserPlaces = () => {
           <LoadingSpinner />
         </div>
       }
-      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />}
     </React.Fragment>
   );
 };
