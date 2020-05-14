a044ef29b9234cc6f164c010430db233f00f663b
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-14 12:55:38 +0600

    sign up image upload ui added

---
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+
+import Button from './Button';
+import './ImageUpload.css';
+
+const ImageUpload = props => {
+    const filePickerRef = useRef();
+
+    const pickedHandler = event => {
+
+    };
+
+    const pickImageHandler = () => {
+        filePickerRef.current.click();
+    };
+
+    return (
+        <div className="form-control">
+            <input
+                id={props.id}
+                ref={filePickerRef}
+                style={{ display: 'none' }}
+                type="file"
+                accept=".jpg, .png, .jpeg"
+                onChange={pickedHandler}
+            />
+            <div className={`image-upload ${props.center && 'center'}`}>
+                <div className="image-upload__preview">
+                    <img src="" alt="Preview" />
+                </div>
+                <Button type="button" onClick={pickImageHandler} >Pick Image</Button>
+            </div>
+        </div>
+    );
+};
+
+export default ImageUpload;
