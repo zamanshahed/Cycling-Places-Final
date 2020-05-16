22efaa351a575e0ed37df52082793c7db70e80ab
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-16 13:00:55 +0600

    frontend and backend validation added for file mime-types before image upload

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -10,14 +10,19 @@ const MIME_TYPE_MAP = {
 const fileUpload = multer({
     limits: 500000,
     storage: multer.diskStorage({
-        destination: (req, file, cb)=>{
-
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/images');
         },
-        filename: (req, file, cb)=>{
-            const ext= MIME_TYPE_MAP[file.mimetype];
-            cb(null, uuid()+'.'+ext);
+        filename: (req, file, cb) => {
+            const ext = MIME_TYPE_MAP[file.mimetype];
+            cb(null, uuid() + '.' + ext);
         },
     }),
+    fileFilter: (req, file, cb) => { 
+        const isValid = !!MIME_TYPE_MAP[file.mimetype];
+        let error = isValid ? null : new Error('Invalid mime(file extention) type!');
+        cb(error, isValid);
+     }
 });
 
 module.exports = fileUpload;
