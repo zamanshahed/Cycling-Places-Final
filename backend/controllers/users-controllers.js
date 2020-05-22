80f128a10d63999e7cdd365f61cb342f3bf8282c
zamanshahed <shaheduz.zaman@northsouth.edu>
2020-05-23 00:43:40 +0600

    token creation added for login and sign up

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtocken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -72,7 +73,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) }); // createdUser includes the PW
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'super_secret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError('Sign up failed, token error, try again later.', 500);
+    return next(error);
+  }
+
+  res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token }); // createdUser includes the PW
 };
 
 const login = async (req, res, next) => {
@@ -103,7 +116,30 @@ const login = async (req, res, next) => {
     isValidPassword = await bcrypt.compare(password, existingUser.password);
   } catch (err) {
     const error = new HttpError('Error: Could not log you in, please check credentials and try again.', 500);
+    return next(error);
   }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'super_secret_dont_share',
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    const error = new HttpError('Login failed, token error, try again later.', 500);
+    return next(error);
+  }
+
 
   res.json({ message: 'Logged in!', user: existingUser.toObject({ getters: true }) });
 };
