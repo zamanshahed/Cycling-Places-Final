db868df291d746d9b068223ddde6b7799e1b10ca
Shahed <shaheduz.zaman@northsouth.edu>
2020-05-24 13:40:36 +0600

    backend router protection with new auth-check middleware

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyCDsl3eVL9I9OJqrkDPWE2-7fux35cShJE';
+const API_KEY = '';
 
 async function getCoordsForAddress(address) {
   return {
